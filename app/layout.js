cbc63e091804d48709495ca9a74ccf62c7942172
rafat22 <rafat22@navgurukul.org>
2024-12-29 22:23:42 +0530

    domain change

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "micro-sonic.online",
-  description: "hewlett-printer-solution",
+  description: "micro-sonic.online",
 };
 
 export default function RootLayout({ children }) {
